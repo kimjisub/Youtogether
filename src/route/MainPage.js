afa28718aa5e6399dae34803d406e4bc00c43e4c
0226daniel <0226daniel@gmail.com>
2020-03-09 01:51:29 +0900

    Get Youtube Time

--- src/route/MainPage.js
+++ src/route/MainPage.js
@@ -1,44 +1,41 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
 import './MainPage.scss'
 import { firestore } from '../firebase'
 
-class App extends Component {
-	constructor() {
-		super()
+function App(prop) {
+	const fs = firestore()
 
-		this.fs = firestore()
+	function createRoom() {
+		fs.collection('Room')
+			.add({
+				url: '',
+				playing: false,
+				playTime: {
+					currentTime: 0,
+					startAt: 0
+				}
+			})
+			.then(docRef => {
+				prop.onCreated(docRef.id)
+			})
+			.catch(err => {
+				alert(err)
+			})
 	}
 
-	render() {
-		return (
-			<div className="MainPage">
-				<Button
-					raised
-					ripple
-					onClick={() => {
-						this.fs
-							.collection('Room')
-							.add({
-								url: '',
-								playing: false,
-								playTime: {
-									currentTime: 0,
-									startAt: 0
-								}
-							})
-							.then(docRef => {
-								this.props.onCreated(docRef.id)
-							})
-							.catch(err => {
-								this.setState({ err: err })
-							})
-					}}>
-					새로 만들기
-				</Button>
-			</div>
-		)
-	}
+	return (
+		<div className="MainPage">
+			<Button
+				raised
+				ripple
+				onClick={() => {
+					createRoom()
+				}}>
+				새로 만들기
+			</Button>
+		</div>
+	)
 }
 
 export default App
